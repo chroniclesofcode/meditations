2928465799fb461028bdec4e7cf27201f9af27d2
Donald Tang <donald.tang.email@googlemail.com>
2023-11-19 11:22:40 +1100

    live

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'chroniclesofcode', // Usually your GitHub org/user name.
   projectName: 'blog', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
